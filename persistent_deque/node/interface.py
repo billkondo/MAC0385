cc30d8b0441af6c6c40a932fd580f775f4ad25c2
William Hideki Kondo <willkondo@gmail.com>
2021-08-27 20:14:58 -0300

    Implementing LowestCommonAncestor

--- persistent_deque/node/interface.py
+++ persistent_deque/node/interface.py
@@ -28,10 +28,7 @@ def LevelAncestor(k : int, node: Node) -> Node:
   if type(k) != int:
     raise TypeError('k is not an integer')
 
-  if node == None:
-    raise ValueError('node is None')
-
-  if type(node) != Node:
+  if node == None or type(node) != Node:
     raise TypeError('node is not a Node')
 
   if k > Depth(node):
@@ -46,3 +43,28 @@ def LevelAncestor(k : int, node: Node) -> Node:
       node = node.parent
   
   return node
+
+def LowestCommonAncestor(u : Node, v : Node) -> Node:
+  if u == None or type(u) != Node:
+    raise TypeError('u is not a Node')
+
+  if v == None or type(v) != Node:
+    raise TypeError('v is not a Node')
+
+  if Depth(u) > Depth(v):
+    u, v = v, u
+  
+  v = LevelAncestor(Depth(v) - Depth(u), v)
+
+  if v == u:
+    return u
+
+  while u.parent != v.parent:
+    if u.jump != v.jump:
+      u = u.jump
+      v = v.jump
+    else:
+      u = u.parent
+      v = v.parent
+
+  return u.parent
--- persistent_deque/tests/test_node_interface.py
+++ persistent_deque/tests/test_node_interface.py
@@ -3,7 +3,7 @@ import unittest
 
 from persistent_deque.node import Node
 from persistent_deque.node.interface import Depth, AddLeaf \
-  ,LevelAncestor 
+  , LevelAncestor, LowestCommonAncestor
 
 class NodeInterfaceTest(unittest.TestCase):
   def setUp(self):
@@ -54,6 +54,31 @@ class NodeInterfaceTest(unittest.TestCase):
 
   def test_level_ancestor_raises(self):
     self.assertRaises(TypeError, lambda: LevelAncestor('invalid', None))
-    self.assertRaises(ValueError, lambda: LevelAncestor(0, None))
+    self.assertRaises(TypeError, lambda: LevelAncestor(0, None))
     self.assertRaises(TypeError, lambda: LevelAncestor(0, 'invalid'))
     self.assertRaises(ValueError, lambda: LevelAncestor(10, self.node5))
+
+  def test_lowest_common_ancestor(self):
+    root  = AddLeaf(0, None)
+    node1 = AddLeaf(1, root)
+    node2 = AddLeaf(2, root)
+    node3 = AddLeaf(3, node1)
+    node4 = AddLeaf(4, node1)
+    node5 = AddLeaf(5, node2)
+    node6 = AddLeaf(4, node4)
+    node7 = AddLeaf(7, node5)
+    node8 = AddLeaf(8, node5)
+
+    self.assertEqual(LowestCommonAncestor(root, root), root)
+    self.assertEqual(LowestCommonAncestor(node3, node6), node1)
+    self.assertEqual(LowestCommonAncestor(node4, node7), root)
+    self.assertEqual(LowestCommonAncestor(node7, node8), node5)
+    self.assertEqual(LowestCommonAncestor(node5, node5), node5)
+    self.assertEqual(LowestCommonAncestor(node2, node7), node2)
+    self.assertEqual(LowestCommonAncestor(node6, node1), node1)
+    self.assertEqual(LowestCommonAncestor(node6, node8), root)
+    self.assertEqual(LowestCommonAncestor(node1, node5), root)
+
+  def test_lowest_common_ancestor_raises(self):
+    self.assertRaises(TypeError, lambda: LowestCommonAncestor(None, None))
+    self.assertRaises(TypeError, lambda: LowestCommonAncestor(self.root, None))
