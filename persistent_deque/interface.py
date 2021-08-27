c023638fa04d05196566865d0fc1d70847fe0058
William Hideki Kondo <willkondo@gmail.com>
2021-08-27 20:54:41 -0300

    Adding validation for deque interface methods

--- persistent_deque/interface.py
+++ persistent_deque/interface.py
@@ -2,15 +2,28 @@ from persistent_deque.node import AddLeaf, Depth\
   , LevelAncestor, LowestCommonAncestor
 from persistent_deque.deque import Deque
 
+def deque_validator(function):
+  def wrapper(*args):
+    deque = args[0]
+    if deque == None or type(deque) != Deque:
+      raise TypeError('deque is not a Deque')
+
+    return function(*args)
+
+  return wrapper
+
 def NewDeque() -> Deque:
   return Deque(None, None)
 
+@deque_validator
 def Front(deque : Deque):
   return deque.first.value
 
+@deque_validator
 def Back(deque : Deque):
   return deque.last.value
 
+@deque_validator
 def PushFront(deque: Deque, value : int) -> Deque:
   if deque.first == None:
     root = AddLeaf(value, None)
@@ -18,13 +31,22 @@ def PushFront(deque: Deque, value : int) -> Deque:
   else:
     return Deque(AddLeaf(value, deque.first), deque.last)
 
+@deque_validator
 def Swap(deque:Deque) -> Deque:
   return Deque(deque.last, deque.first)
 
+@deque_validator
 def PushBack(deque: Deque, value) -> Deque:
   return Swap(PushFront(Swap(deque), value))
 
+@deque_validator
 def Kth(deque : Deque, k : int) -> Deque:
+  if k == None or type(k) != int:
+    raise TypeError('k is not an integer')
+
+  if k < 0:
+    raise ValueError('k is less than zero')
+
   u = deque.first
   v = deque.last
 
--- persistent_deque/tests/test_interface.py
+++ persistent_deque/tests/test_interface.py
@@ -20,8 +20,14 @@ class InterfaceTest(unittest.TestCase):
   def test_front(self):
     self.assertEqual(Front(self.deque), self.node1.value)
 
+    self.assertRaises(TypeError, lambda: Front(None))
+    self.assertRaises(TypeError, lambda: Front('invalid'))
+
   def test_back(self):
     self.assertEqual(Back(self.deque), self.node2.value)
+
+    self.assertRaises(TypeError, lambda: Back(None))
+    self.assertRaises(TypeError, lambda: Back('invalid'))
 
   def test_push_front(self):
     new_deque = PushFront(self.deque, 3)
@@ -32,6 +38,9 @@ class InterfaceTest(unittest.TestCase):
     self.assertEqual(new_deque_first.value, 3)
     self.assertEqual(new_deque_first.parent, self.node1)
     self.assertEqual(new_deque_first.depth, 1)
+
+    self.assertRaises(TypeError, lambda: PushFront(None, 3))
+    self.assertRaises(TypeError, lambda: PushFront('invalid', 3))
 
   def test_push_front_emtpy(self):
     new_deque = PushFront(NewDeque(), 1)
@@ -48,6 +57,9 @@ class InterfaceTest(unittest.TestCase):
     self.assertEqual(deque_swap.first, self.node2)
     self.assertEqual(deque_swap.last, self.node1)
 
+    self.assertRaises(TypeError, lambda: Swap(None))
+    self.assertRaises(TypeError, lambda: Swap('invalid'))
+
   def test_push_back(self):
     new_deque = PushBack(self.deque, 3)
     new_deque_last = new_deque.last
@@ -57,6 +69,9 @@ class InterfaceTest(unittest.TestCase):
     self.assertEqual(new_deque_last.value, 3)
     self.assertEqual(new_deque_last.parent, self.node2)
     self.assertEqual(new_deque_last.depth, 1)
+
+    self.assertRaises(TypeError, lambda: PushBack(None, 3))
+    self.assertRaises(TypeError, lambda: PushBack('invalid', 3))
 
   def test_push_back_empty(self):
     new_deque = PushBack(NewDeque(), 1)
@@ -99,3 +114,7 @@ class PopInterfaceTest(unittest.TestCase):
   def test_kth_raises(self):
     self.assertRaises(ValueError, lambda: Kth(self.deque5, 5))
     self.assertRaises(ValueError, lambda: Kth(self.deque4, 4))
+    self.assertRaises(TypeError, lambda: Kth(None, 1))
+    self.assertRaises(TypeError, lambda: Kth('invalid', 1))
+    self.assertRaises(TypeError, lambda: Kth(self.deque2, 'invalid'))
+    self.assertRaises(ValueError, lambda: Kth(self.deque5, -1))
