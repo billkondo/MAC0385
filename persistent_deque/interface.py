e9d7e6269ebe0d51480415ea4c910635e6f8c8c5
William Hideki Kondo <willkondo@gmail.com>
2021-08-27 15:47:52 -0300

    Type hint for NewDeque / Implementing PushFront

--- persistent_deque/interface.py
+++ persistent_deque/interface.py
@@ -1,8 +1,7 @@
-from typing import Deque
-
+from persistent_deque.node import Node
 from persistent_deque.deque import Deque
 
-def NewDeque():
+def NewDeque() -> Deque:
   return Deque(None, None)
 
 def Front(deque : Deque):
@@ -10,3 +9,10 @@ def Front(deque : Deque):
 
 def Back(deque : Deque):
   return deque.last.value
+
+def PushFront(deque: Deque, value) -> Deque:
+  if deque.first == None:
+    root = Node(value, None, 0)
+    return Deque(root, root)
+  else:
+    return Deque(Node(value, deque.first, deque.first.depth + 1), deque.last)
--- persistent_deque/tests/test_interface.py
+++ persistent_deque/tests/test_interface.py
@@ -2,7 +2,7 @@ import unittest
 
 from persistent_deque.node import Node
 from persistent_deque.deque import Deque
-from persistent_deque.interface import NewDeque, Front, Back
+from persistent_deque.interface import NewDeque, Front, Back, PushFront
 
 class InterfaceTest(unittest.TestCase):
   def setUp(self):
@@ -21,3 +21,22 @@ class InterfaceTest(unittest.TestCase):
 
   def test_back(self):
     self.assertEqual(Back(self.deque), self.node2.value)
+
+  def test_push_front(self):
+    new_deque = PushFront(self.deque, 3)
+    new_deque_first = new_deque.first
+
+    self.assertEqual(new_deque.last, self.node2)
+
+    self.assertEqual(new_deque_first.value, 3)
+    self.assertEqual(new_deque_first.parent, self.node1)
+    self.assertEqual(new_deque_first.depth, self.node1.depth + 1)
+
+  def test_push_front_emtpy(self):
+    new_deque = PushFront(NewDeque(), 1)
+
+    self.assertEqual(new_deque.first, new_deque.last)
+
+    self.assertEqual(new_deque.first.value, 1)
+    self.assertEqual(new_deque.first.parent, None)
+    self.assertEqual(new_deque.first.depth, 0)
