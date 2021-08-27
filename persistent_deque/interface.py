be73c8f562a3188484861b5ed229bc9396b4537f
William Hideki Kondo <willkondo@gmail.com>
2021-08-27 15:16:57 -0300

    Implementing Front and Back methods

--- persistent_deque/interface.py
+++ persistent_deque/interface.py
@@ -4,3 +4,9 @@ from persistent_deque.deque import Deque
 
 def NewDeque():
   return Deque(None, None)
+
+def Front(deque : Deque):
+  return deque.first.value
+
+def Back(deque : Deque):
+  return deque.last.value
--- persistent_deque/tests/test_interface.py
+++ persistent_deque/tests/test_interface.py
@@ -1,10 +1,23 @@
 import unittest
 
-from persistent_deque.interface import NewDeque
+from persistent_deque.node import Node
+from persistent_deque.deque import Deque
+from persistent_deque.interface import NewDeque, Front, Back
 
 class InterfaceTest(unittest.TestCase):
+  def setUp(self):
+    self.node1 = Node(1, None, 0)
+    self.node2 = Node(2, None, 0)
+    self.deque = Deque(self.node1, self.node2)
+
   def test_new_deque(self):
     deque = NewDeque()
 
     self.assertEqual(deque.first, None)
     self.assertEqual(deque.last, None)
+
+  def test_front(self):
+    self.assertEqual(Front(self.deque), self.node1.value)
+
+  def test_back(self):
+    self.assertEqual(Back(self.deque), self.node2.value)
