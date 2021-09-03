25a5678b75c918875c2cce3347992807dabe5acc
William Hideki Kondo <willkondo@gmail.com>
2021-09-03 12:39:18 -0300

    Creating BST constructor

---
+++ persistent_bst/bst.py
@@ -0,0 +1,8 @@
+from persistent_bst.node.node import Node
+
+class BST:
+  def __init__(self, root: Node = None):
+    if root != None and type(root) != Node:
+      raise TypeError('root is not a Node')
+
+    self.root = root
