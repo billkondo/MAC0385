b8440f2c9c716f1d0a616ba03fb787d0d1b2f713
William Hideki Kondo <willkondo@gmail.com>
2021-09-03 21:45:55 -0300

    Insert implementation

--- persistent_bst/interface.py
+++ persistent_bst/interface.py
@@ -1,4 +1,21 @@
 from persistent_bst.bst import BST
+from persistent_bst.node.interface import CopyNode, InsertNode
 
 def NewBST() -> BST:
   return BST()
+
+def CopyBST(b: BST) -> BST:
+  if b == None:
+    raise ValueError('b is None')
+
+  root_copy = CopyNode(b.root)
+  return BST(root_copy)
+
+def Insert(b: BST, value) -> BST:
+  if b == None:
+    raise ValueError('b is None')
+
+  if type(b) != BST:
+    raise TypeError('b is not a BST')
+
+  return BST(InsertNode(b.root, value))
--- persistent_bst/tests/test_interface.py
+++ persistent_bst/tests/test_interface.py
@@ -1,8 +1,37 @@
 import unittest
 
-from persistent_bst.interface import NewBST
+from persistent_bst.bst import BST
+from persistent_bst.interface import NewBST, CopyBST, Insert
+from persistent_bst.node.node import Node
 
 class InterfaceTest(unittest.TestCase):
   def test_new_bst(self):
     bst = NewBST()
     self.assertEqual(bst.root, None)
+
+  def test_copy_bst(self):
+    self.assertRaises(ValueError, lambda: CopyBST(None))
+
+    bst = BST(Node(5))
+    bst_copy = CopyBST(bst)
+
+    self.assertEqual(bst.root.value, bst_copy.root.value)
+    self.assertNotEqual(bst.root, bst_copy.root)
+
+  def test_insert(self):
+    self.assertRaises(TypeError, lambda: Insert('invalid', 5))
+    self.assertRaises(ValueError, lambda: Insert(None, 5))
+
+    bst_0 = NewBST()
+    
+    bst_1 = Insert(bst_0, 5)
+    self.assertEqual(bst_1.root.value, 5)
+    self.assertNotEqual(bst_0.root, bst_1.root)
+
+    bst_2 = Insert(bst_1, 10)
+    self.assertEqual(bst_2.root.right.value, 10)
+    self.assertNotEqual(bst_1.root, bst_2.root)
+
+    bst_3 = Insert(bst_2, 5)
+    self.assertEqual(bst_3.root.left.value, 5)
+    self.assertNotEqual(bst_2.root, bst_3.root)
