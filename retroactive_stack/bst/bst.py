629800e39d8213fe0f4b2af6e186a3582e5b9d1d
William Hideki Kondo <willkondo@gmail.com>
2021-09-11 20:07:32 -0300

    Creating BST interface

---
+++ retroactive_stack/bst/bst.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from retroactive_stack.operation import Operation
+
+class BST(ABC):
+  @abstractmethod
+  def insert(self, key: int, operation: Operation):
+    pass
+
+  @abstractmethod
+  def delete(self, key: int):
+    pass
+
+  @abstractmethod
+  def count_less_than(self, key: int) -> int:
+    pass
+
+  @abstractmethod
+  def kth(self, k: int) -> Any:
+    pass
