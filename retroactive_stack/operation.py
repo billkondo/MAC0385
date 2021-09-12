bf3d5d3eec815a86f3bd690b6e6bb685050d36ca
William Hideki Kondo <willkondo@gmail.com>
2021-09-11 23:04:11 -0300

    Fixing linting

--- retroactive_stack/operation.py
+++ retroactive_stack/operation.py
@@ -1,15 +1,16 @@
 from typing import Any
 
+
 class Operation:
-  def __init__(self, type = None, value = None):
-    if type != -1 and type != 1:
-      raise ValueError('type is not -1 neither 1')
+    def __init__(self, type=None, value=None):
+        if type != -1 and type != 1:
+            raise ValueError("type is not -1 neither 1")
 
-    if type == -1 and value != None:
-      raise ValueError('type is -1, but value is not None')
+        if type == -1 and value is not None:
+            raise ValueError("type is -1, but value is not None")
 
-    if type == 1 and value == None:
-      raise ValueError('type is 1, but value is None')
+        if type == 1 and value is None:
+            raise ValueError("type is 1, but value is None")
 
-    self.type: int = type
-    self.value: Any = value
+        self.type: int = type
+        self.value: Any = value
