c5c12ffa7f138c94c6659788a42e6317ffc62312
William Hideki Kondo <willkondo@gmail.com>
2021-09-11 23:44:13 -0300

    Implementing AddPush and AddPop

--- retroactive_stack/interface.py
+++ retroactive_stack/interface.py
@@ -1,5 +1,45 @@
+from typing import Any
+
+from retroactive_stack.operation import Operation
 from retroactive_stack.stack import Stack
+
+
+def stack_validator(function):
+    def wrapper(*args):
+        stack = args[0]
+        time = args[1]
+
+        if type(stack) is not Stack:
+            raise TypeError("deque is not a Stack")
+
+        if type(time) is not int:
+            raise TypeError("time is not a int")
+
+        return function(*args)
+
+    return wrapper
 
 
 def NewStack(bst=None) -> Stack:
     return Stack(bst)
+
+
+@stack_validator
+def AddPush(stack: Stack, time: int, value: Any):
+    stack.bst.insert(
+        key=time,
+        operation=Operation(
+            type=1,
+            value=value,
+        ),
+    )
+
+
+@stack_validator
+def AddPop(stack: Stack, time: int):
+    stack.bst.insert(
+        key=time,
+        operation=Operation(
+            type=-1,
+        ),
+    )
--- retroactive_stack/tests/test_interface.py
+++ retroactive_stack/tests/test_interface.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import Mock
 
 from retroactive_stack.bst.bst import BST
-from retroactive_stack.interface import NewStack
+from retroactive_stack.interface import AddPop, AddPush, NewStack
 
 
 class InterfaceTest(unittest.TestCase):
@@ -10,3 +10,26 @@ class InterfaceTest(unittest.TestCase):
         bst = Mock(BST)
         self.assertEqual(NewStack(bst).bst, bst)
         self.assertIsInstance(NewStack().bst, BST)
+
+    def test_add_push(self):
+        self.assertRaises(TypeError, lambda: AddPush(None, 3, "A"))
+        self.assertRaises(TypeError, lambda: AddPush("invalid", 1, "B"))
+        self.assertRaises(
+            TypeError,
+            lambda: AddPush(NewStack(), "invalid", "C"),
+        )
+
+        bst = Mock(BST)
+        stack = NewStack(bst)
+
+        AddPush(stack, 5, "A")
+
+        bst.insert.assert_called_once()
+
+    def test_add_pop(self):
+        bst = Mock(BST)
+        stack = NewStack(bst)
+
+        AddPop(stack, 10)
+
+        bst.insert.assert_called_once()
