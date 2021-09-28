c16b2a2526bf4de991a492f8d63ffec50f667c01
William Hideki Kondo <willkondo@gmail.com>
2021-09-27 22:32:51 -0300

    Make Operation key optional

--- retroactive_heap/operations_bst/operation.py
+++ retroactive_heap/operations_bst/operation.py
@@ -1,16 +1,21 @@
 class Operation:
-    def __init__(self, time=None, key=None, type=None):
+    def __init__(
+        self,
+        time=None,
+        type=None,
+        key=None,
+    ):
         if not isinstance(time, int):
             raise TypeError("time is not a int")
-
-        if not isinstance(key, int):
-            raise TypeError("key in not a int")
 
         if not isinstance(type, int):
             raise TypeError("type is not a int")
 
         if type < -1 or type > 1:
             raise ValueError("type is not in range [-1, 1]")
+
+        if key is not None and not isinstance(key, int):
+            raise TypeError("key in not a int")
 
         self.time: int = time
         self.key: int = key
--- retroactive_heap/tests/test_operation.py
+++ retroactive_heap/tests/test_operation.py
@@ -6,12 +6,10 @@ from retroactive_heap.operations_bst.operation import Operation
 class TestOperation(unittest.TestCase):
     def test_operation(self):
         self.assertRaises(TypeError, lambda: Operation(time="20"))
-        self.assertRaises(TypeError, lambda: Operation(time=1, key="123"))
         self.assertRaises(
             TypeError,
             lambda: Operation(
                 time=1,
-                key=10,
                 type="2",
             ),
         )
@@ -31,8 +29,27 @@ class TestOperation(unittest.TestCase):
                 type=-10,
             ),
         )
+        self.assertRaises(
+            TypeError,
+            lambda: Operation(
+                time=1,
+                type=0,
+                key="123",
+            ),
+        )
 
-        operation = Operation(15, 10, 0)
+        operation = Operation(
+            time=15,
+            type=0,
+            key=10,
+        )
         self.assertEqual(operation.time, 15)
         self.assertEqual(operation.key, 10)
         self.assertEqual(operation.type, 0)
+
+        operation_empty_key = Operation(
+            time=12,
+            type=-1,
+        )
+        self.assertEqual(operation_empty_key.time, 12)
+        self.assertEqual(operation_empty_key.type, -1)
