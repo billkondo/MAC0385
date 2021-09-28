4dd44659e38d4851d71ef2f64cabd244b8f6e30c
William Hideki Kondo <willkondo@gmail.com>
2021-09-27 22:22:55 -0300

    Add field time to Operation

--- retroactive_heap/operations_bst/operation.py
+++ retroactive_heap/operations_bst/operation.py
@@ -1,13 +1,17 @@
 class Operation:
-    def __init__(self, key=None, type=None):
+    def __init__(self, time=None, key=None, type=None):
+        if not isinstance(time, int):
+            raise TypeError("time is not a int")
+
         if not isinstance(key, int):
-            raise TypeError('key in not a int')
+            raise TypeError("key in not a int")
 
         if not isinstance(type, int):
-            raise TypeError('type is not a int')
+            raise TypeError("type is not a int")
 
         if type < -1 or type > 1:
-            raise TypeError('type is not in range [-1, 1]')
+            raise ValueError("type is not in range [-1, 1]")
 
+        self.time: int = time
         self.key: int = key
         self.type: int = type
--- retroactive_heap/tests/test_operation.py
+++ retroactive_heap/tests/test_operation.py
@@ -5,11 +5,34 @@ from retroactive_heap.operations_bst.operation import Operation
 
 class TestOperation(unittest.TestCase):
     def test_operation(self):
-        self.assertRaises(TypeError, lambda: Operation(key="123"))
-        self.assertRaises(TypeError, lambda: Operation(type="2"))
-        self.assertRaises(TypeError, lambda: Operation(type=10))
-        self.assertRaises(TypeError, lambda: Operation(type=-10))
+        self.assertRaises(TypeError, lambda: Operation(time="20"))
+        self.assertRaises(TypeError, lambda: Operation(time=1, key="123"))
+        self.assertRaises(
+            TypeError,
+            lambda: Operation(
+                time=1,
+                key=10,
+                type="2",
+            ),
+        )
+        self.assertRaises(
+            ValueError,
+            lambda: Operation(
+                time=1,
+                key=10,
+                type=10,
+            ),
+        )
+        self.assertRaises(
+            ValueError,
+            lambda: Operation(
+                time=1,
+                key=10,
+                type=-10,
+            ),
+        )
 
-        operation = Operation(key=10, type=0)
+        operation = Operation(15, 10, 0)
+        self.assertEqual(operation.time, 15)
         self.assertEqual(operation.key, 10)
         self.assertEqual(operation.type, 0)
