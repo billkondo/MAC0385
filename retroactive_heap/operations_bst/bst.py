dfed2d9826cb119d0ef511b204ee4983f4b787a7
William Hideki Kondo <willkondo@gmail.com>
2021-09-27 23:49:01 -0300

    Create BST interface

---
+++ retroactive_heap/operations_bst/bst.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractclassmethod, abstractmethod
+
+from retroactive_heap.operations_bst.operation import Operation
+
+
+class BST(ABC):
+    @abstractmethod
+    def insert(self, time: int = None, type: int = None, key: int = None):
+        pass
+
+    @abstractclassmethod
+    def delete(self, time: int):
+        pass
+
+    @abstractclassmethod
+    def previous_bridge_time(self, time: int) -> int:
+        pass
+
+    @abstractclassmethod
+    def next_bridge_time(self, time: int) -> int:
+        pass
+
+    @abstractclassmethod
+    def max(self, time: int) -> Operation:
+        pass
