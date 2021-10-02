9a1e2cf9b6fc93c9542a04fbba94c3ff92ac7851
William Hideki Kondo <willkondo@gmail.com>
2021-10-02 13:32:12 -0300

    Add Insert, Delete, Find and Min to AVL interface

--- avl/interface.py
+++ avl/interface.py
@@ -1,6 +1,80 @@
 from avl.avl_node import AVLNode
 
 
+def Insert(node: AVLNode, newNode: AVLNode) -> AVLNode:
+    if not isinstance(newNode, AVLNode):
+        raise TypeError("newNode is not a AVLNode")
+
+    if node is None:
+        return newNode
+
+    if not isinstance(node, AVLNode):
+        raise TypeError("node is not a AVLNode")
+
+    if newNode.key <= node.key:
+        node.L = Insert(node.L, newNode)
+    else:
+        node.R = Insert(node.R, newNode)
+
+    node.update()
+    return Balance(node)
+
+
+def Delete(node: AVLNode, key: int) -> AVLNode:
+    if not isinstance(node, AVLNode):
+        raise TypeError("node is not a AVLNode")
+
+    if node.key == key:
+        if node.R is not None:
+            min_node = Min(node.R)
+            min_node.R = Delete(node.R, min_node.key)
+            min_node.L = node.L
+            min_node.update()
+            return Balance(min_node)
+
+        if node.L is not None:
+            return node.L
+
+        return None
+
+    if key < node.key:
+        node.L = Delete(node.L, key)
+    else:
+        node.R = Delete(node.R, key)
+
+    node.update()
+    return Balance(node)
+
+
+def Find(node: AVLNode, key: int) -> AVLNode:
+    if node is None:
+        return None
+
+    if not isinstance(node, AVLNode):
+        raise TypeError("node is not a AVLNode")
+
+    if node.key == key:
+        return node
+
+    if node.L is not None and key < node.key:
+        return Find(node.L, key)
+
+    if node.R is not None and node.key < key:
+        return Find(node.R, key)
+
+    return None
+
+
+def Min(node: AVLNode) -> AVLNode:
+    if not isinstance(node, AVLNode):
+        raise TypeError("node is not a AVLNode")
+
+    if node.L is not None:
+        return Min(node.L)
+
+    return node
+
+
 def RotateRight(node: AVLNode) -> AVLNode:
     #          x             y
     #        / |           / |
--- avl/tests/test_interface.py
+++ avl/tests/test_interface.py
@@ -1,7 +1,17 @@
 import unittest
+from math import inf
+from typing import Dict
 
-from avl.avl_node import AVLNode
-from avl.interface import Balance, RotateLeft, RotateRight
+from avl.avl_node import AVLNode, Height
+from avl.interface import (
+    Balance,
+    Delete,
+    Find,
+    Insert,
+    Min,
+    RotateLeft,
+    RotateRight,
+)
 
 
 class TestInterface(unittest.TestCase):
@@ -158,3 +168,114 @@ class TestInterface(unittest.TestCase):
         self.assertEqual(node, xRL)
         self.assertEqual(node.L, x)
         self.assertEqual(node.R, xR)
+
+    def test_min(self):
+        self.assertRaises(TypeError, lambda: Min("invalid"))
+
+        node10 = AVLNode(key=10)
+        node5 = AVLNode(key=5)
+        node15 = AVLNode(key=15)
+        node12 = AVLNode(key=12)
+
+        root = Insert(None, node10)
+        root = Insert(root, node5)
+        root = Insert(root, node15)
+        root = Insert(root, node12)
+
+        self.assertEqual(Min(root).key, 5)
+        self.assertEqual(Min(node5).key, 5)
+        self.assertEqual(Min(node15).key, 12)
+        self.assertEqual(Min(node12).key, 12)
+
+    def test_insert_delete_find(self):
+        self.assertRaises(TypeError, lambda: Insert("invalid", AVLNode(key=5)))
+        self.assertRaises(TypeError, lambda: Insert(AVLNode(key=5), "invalid"))
+        self.assertRaises(TypeError, lambda: Delete("invalid"))
+        self.assertRaises(TypeError, lambda: Find("invalid", 5))
+
+        def count(node: AVLNode):
+            if node is None:
+                return 0
+
+            return count(node.L) + 1 + count(node.R)
+
+        def check_tree(root: AVLNode):
+            height: Dict[AVLNode, int] = {}
+            height[None] = -1
+
+            balance: Dict[AVLNode, int] = {}
+            balance[None] = 0
+
+            def compute(node: AVLNode):
+                if node is None:
+                    return
+
+                compute(node.L)
+                compute(node.R)
+
+                height[node] = max(height[node.L], height[node.R]) + 1
+                balance[node] = balance[node.R] - balance[node.R]
+
+            def check(node: AVLNode, left_key=-inf, right_key=inf):
+                if node is None:
+                    return
+
+                if height[node] != Height(node):
+                    raise ValueError("unmatched height")
+
+                if balance[node] != Balance(node):
+                    raise ValueError("unmatched balance")
+
+                if node.key < left_key or right_key < node.key:
+                    raise ValueError("BST invariant broken")
+
+                if balance[node] < -1 or balance[node] > 1:
+                    raise ValueError("AVL invariant broken")
+
+                check(node.L, left_key, node.key)
+                check(node.R, node.key, right_key)
+
+            compute(root)
+
+        def insert(root: AVLNode, key: int):
+            self.assertIsNone(Find(root, key))
+            root = Insert(root, AVLNode(key))
+            self.assertEqual(Find(root, key).key, key)
+
+            return root
+
+        def delete(root: AVLNode, key: int):
+            self.assertEqual(Find(root, key).key, key)
+            root = Delete(root, key)
+            self.assertIsNone(Find(root, key))
+
+            return root
+
+        root = None
+        for key in range(1, 100):
+            root = insert(root, key)
+
+            self.assertEqual(count(root), key)
+            check_tree(root)
+
+        for key in range(1, 100):
+            root = delete(root, key)
+
+            self.assertEqual(count(root), 99 - key)
+            check_tree(root)
+
+        self.assertIsNone(root)
+
+        for key in range(999, 0, -1):
+            root = insert(root, key)
+
+            self.assertEqual(count(root), 999 - key + 1)
+            check_tree(root)
+
+        size = 999
+        for key in range(999, 1, -5):
+            root = delete(root, key)
+            size -= 1
+
+            self.assertEqual(count(root), size)
+            check_tree(root)
