82c6612dd8d59ae2893b71237cd9c4c2f8a43b39
William Hideki Kondo <willkondo@gmail.com>
2021-12-18 13:56:46 -0300

    Add linear lcp array build implementation

--- suffix_array/tests/test_build_lcp_array.py
+++ suffix_array/tests/test_build_lcp_array.py
@@ -1,28 +1,34 @@
 import unittest
+from typing import List
 
-from parameterized import parameterized_class
-from suffix_array import build_lcp_array, build_suffix_array
+from parameterized import parameterized, parameterized_class
+from suffix_array import build_lcp_array, build_lcp_array_linear, build_suffix_array
 
 
 @parameterized_class(
     [
         {
-            "text": "banana",
-            "expected": [0, 0, 1, 3, 0, 0, 2],
+            "build_lcp_array": lambda _: build_lcp_array,
         },
         {
-            "text": "abracadabra",
-            "expected": [0, 0, 1, 4, 1, 1, 0, 3, 0, 0, 0, 2],
-        },
-        {
-            "text": "AAACCTTTGCGACC",
-            "expected": [0, 0, 2, 1, 3, 0, 1, 2, 1, 1, 0, 1, 0, 1, 2],
+            "build_lcp_array": lambda _: build_lcp_array_linear,
         },
     ]
 )
 class TestBuildLCPArray(unittest.TestCase):
-    def test_build_lcp_array_01(self):
-        suffix_array = build_suffix_array(self.text)
-        lcp_array = build_lcp_array(self.text, suffix_array)
+    def _build_lcp_array(self, text: str, suffix_array: List[int]) -> List[int]:
+        callback = self.build_lcp_array()
+        return callback(text, suffix_array)
 
-        self.assertEqual(lcp_array, self.expected)
+    @parameterized.expand(
+        [
+            ["banana", [0, 0, 1, 3, 0, 0, 2]],
+            ["abracadabra", [0, 0, 1, 4, 1, 1, 0, 3, 0, 0, 0, 2]],
+            ["AAACCTTTGCGACC", [0, 0, 2, 1, 3, 0, 1, 2, 1, 1, 0, 1, 0, 1, 2]],
+        ]
+    )
+    def test_build_lcp_array_01(self, text: str, expected: List[int]):
+        suffix_array = build_suffix_array(text)
+        lcp_array = self._build_lcp_array(text, suffix_array)
+
+        self.assertEqual(lcp_array, expected)
